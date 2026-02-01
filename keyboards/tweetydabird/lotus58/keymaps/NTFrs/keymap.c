142b2dc937b720498d6e061cb74dc5e695631b84
NTFrs <nahuel.foresta@gmail.com>
2026-02-01 10:30:07 -0800

    latest change

--- keyboards/tweetydabird/lotus58/keymaps/NTFrs/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/NTFrs/keymap.c
@@ -7,24 +7,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         QK_GESC,         KC_1, KC_2,    KC_3,    KC_4,  KC_5,        KC_MPLY,          KC_MUTE, KC_6,    KC_7,    KC_8, KC_9,      KC_0, KC_MINS,
         KC_GRV ,         KC_Q, KC_W,    KC_E,    KC_R,  KC_T,                                   KC_Y,    KC_U,    KC_I, KC_O,      KC_P, KC_LBRC,
-        KC_TAB ,         KC_A, KC_S,    KC_D,    KC_F,  KC_G,                                   KC_H,    KC_J,    KC_K, KC_L,   KC_SCLN, KC_QUOT,
-        LCTL_T(KC_LEFT), KC_Z, KC_X,    KC_C,    KC_V,  KC_B,        KC_DOWN,           KC_UP,  KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RGHT),
-                                     KC_LGUI, KC_LALT, TT(2), LSFT_T(KC_SPC),  RCTL_T(KC_ENT), TT(1), KC_RALT, KC_BSPC
+        KC_LSFT,         KC_A, KC_S,    KC_D,    KC_F,  KC_G,                                   KC_H,    KC_J,    KC_K, KC_L,   KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z, KC_X,    KC_C,    KC_V,  KC_B,        KC_PSCR,           LCTL(LSFT(KC_P)),  KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                                     KC_LGUI, KC_LALT, TT(2), LSFT_T(KC_SPC),  RCTL_T(KC_ENT), TT(1), KC_TAB, KC_BSPC
     ),
 
     [1] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        AS_TOGG, KC_CAPS, KC_PSCR, KC_HOME, KC_END , KC_DEL ,  KC_EQL,
-        KC_BSPC, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                          KC_HASH, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RBRC,
-        _______, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX,                          KC_AMPR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS,
-        KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN,        KC_PGUP, KC_DLR,  XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_LSFT,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        AS_TOGG, KC_CAPS, KC_PSCR, KC_PGUP, KC_PGDN, KC_DEL , XXXXXXX,
+        _______, KC_DEL,  KC_UP,   KC_HOME, KC_END,  XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                    _______, _______, _______, _______,        _______, _______, _______, _______ 
     ),
 
     [2] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        KC_SLEP,   KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        _______, _______, _______, _______, _______, _______,                            KC_7 , KC_8 ,   KC_9 , _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                            KC_4 , KC_5 ,   KC_6 , _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,        _______,   KC_1 , KC_2 ,   KC_3 , _______, _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,        KC_SLEP,   KC_F6, KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_EQL ,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_7 , KC_8 ,   KC_9 , KC_UNDS, KC_LPRN, KC_RBRC,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_4 , KC_5 ,   KC_6 , KC_DQUO, KC_RPRN, KC_INS ,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,        _______,   KC_1 , KC_2 ,   KC_3 , KC_PIPE, KC_BSLS, _______,
                                   _______, _______, _______, _______,         _______, _______, KC_0 , _______
     ),
 
